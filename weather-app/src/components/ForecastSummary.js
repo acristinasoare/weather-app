59ef020d0d6a3b9df943f64f966bb29793ecf733
Cristina Soare <kiti.soare@ymail.com>
2023-08-10 16:56:39 +0100

    converted date into a string and added icons in the ForecastSummary component

--- weather-app/src/components/ForecastSummary.js
+++ weather-app/src/components/ForecastSummary.js
@@ -1,12 +1,30 @@
-import React from "react";
+import iconData from "../data/iconData.json";
 
 function ForecastSummary(props) {
   const { date, description, icon, temperature } = props;
+  const correctDate = new Date(date).toDateString();
+  const weatherCode = `${icon.slice(0, 1)}00`;
+
+  if (icon > 800) {
+    return (
+      <div className="forecast-summary" data-testid="forecast-summary">
+        <div className="forecast-summary__date"> {correctDate}</div>
+        <div className="forecast-summary__icon" data-testid="forecast-icon">
+          <img src={iconData[802]} alt="icon" />
+        </div>
+        <div className="forecast-summary__temperature">
+          {temperature.max}&deg;C
+        </div>
+        <div className="forecast-summary__description">{description}</div>
+      </div>
+    );
+  }
+
   return (
     <div className="forecast-summary" data-testid="forecast-summary">
-      <div className="forecast-summary__date"> {date}</div>
+      <div className="forecast-summary__date"> {correctDate}</div>
       <div className="forecast-summary__icon" data-testid="forecast-icon">
-        {icon}
+        <img src={iconData[weatherCode]} alt="icon" />
       </div>
       <div className="forecast-summary__temperature">
         {temperature.max}&deg;C
